2c2a1270d73ef6162cb7a5b70675f4bf5ba41042
b1llywitant0 <billywitanto@yahoo.co.id>
2025-04-16 20:04:14 +0700

    Refactor: Sticky NavBar

--- src/components/Header/NavigationBar.tsx
+++ src/components/Header/NavigationBar.tsx
@@ -8,32 +8,32 @@ import {
 
 export function NavigationBar() {
   return (
-    <div className="p-4 w-full urbanist-bold">
-      <div className="flex justify-center items-center h-15 w-full isolate aspect-video w-96 rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5">
-        <NavigationMenu>
-          <NavigationMenuList className="flex gap-x-20">
-            <NavigationMenuItem className="w-30">
-              <Link to="/">
-                <NavigationMenuLink className="flex items-center text-xl">
-                  HOME
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem className="w-30">
-              <Link to="/portfolio">
-                <NavigationMenuLink className="flex items-center text-xl">
-                  PORTFOLIOS
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem className="w-30">
-              <Link to="/blog">
-                <NavigationMenuLink className="flex items-center text-xl">
-                  BLOGS
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
+    <div className="p-4 w-full urbanist-bold sticky top-0">
+      <div className="flex justify-center h-15 w-full isolate aspect-video w-96 rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5">
+        <NavigationMenu className='w-full'>
+            <NavigationMenuList className="flex gap-x-10">
+              <NavigationMenuItem className="w-30">
+                <Link to="/">
+                  <NavigationMenuLink className="flex items-center text-l">
+                    HOME
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem className="w-30">
+                <Link to="/portfolio">
+                  <NavigationMenuLink className="flex items-center text-l">
+                    PORTFOLIOS
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem className="w-30">
+                <Link to="/blog">
+                  <NavigationMenuLink className="flex items-center text-l">
+                    BLOGS
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
         </NavigationMenu>
       </div>
     </div>
