2a7e69d5ae2252d14c1bcefcd60c720b67008b71
b1llywitant0 <billywitanto@yahoo.co.id>
2025-04-16 19:39:34 +0700

    Refactor: Font

--- src/components/Header/NavigationBar.tsx
+++ src/components/Header/NavigationBar.tsx
@@ -8,27 +8,27 @@ import {
 
 export function NavigationBar() {
   return (
-    <div className="p-4 w-full">
+    <div className="p-4 w-full urbanist-bold">
       <div className="flex justify-center items-center h-15 w-full isolate aspect-video w-96 rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5">
         <NavigationMenu>
           <NavigationMenuList className="flex gap-x-20">
             <NavigationMenuItem className="w-30">
               <Link to="/">
-                <NavigationMenuLink className="flex items-center">
+                <NavigationMenuLink className="flex items-center text-xl">
                   HOME
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem className="w-30">
               <Link to="/portfolio">
-                <NavigationMenuLink className="flex items-center">
+                <NavigationMenuLink className="flex items-center text-xl">
                   PORTFOLIOS
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem className="w-30">
               <Link to="/blog">
-                <NavigationMenuLink className="flex items-center">
+                <NavigationMenuLink className="flex items-center text-xl">
                   BLOGS
                 </NavigationMenuLink>
               </Link>
