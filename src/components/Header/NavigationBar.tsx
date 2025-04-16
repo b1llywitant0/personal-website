20d33eae39993330a3805b9d7daa2e9e7dae6fca
b1llywitant0 <billywitanto@yahoo.co.id>
2025-04-16 18:35:26 +0700

    Refactor: Glass effect on NavBar

--- src/components/Header/NavigationBar.tsx
+++ src/components/Header/NavigationBar.tsx
@@ -8,26 +8,34 @@ import {
 
 export function NavigationBar() {
   return (
-    <div className="w-full h-16 bg-white flex justify-center">
-      <NavigationMenu>
-        <NavigationMenuList className='flex gap-x-20'>
-          <NavigationMenuItem>
-            <NavigationMenuLink>
-              <Link to="/">Home</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink>
-              <Link to="/portfolio">Portfolio</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink>
-              <Link to="/blog">Blog</Link>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+    <div className="p-4 w-full">
+      <div className="flex justify-center items-center h-15 w-full isolate aspect-video w-96 rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5">
+        <NavigationMenu>
+          <NavigationMenuList className="flex gap-x-20">
+            <NavigationMenuItem className="w-30">
+              <Link to="/">
+                <NavigationMenuLink className="flex items-center">
+                  HOME
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="w-30">
+              <Link to="/portfolio">
+                <NavigationMenuLink className="flex items-center">
+                  PORTFOLIOS
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem className="w-30">
+              <Link to="/blog">
+                <NavigationMenuLink className="flex items-center">
+                  BLOGS
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
     </div>
   )
 }
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 export function Layout() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center">
       <NavigationBar />
       <Outlet />
     </div>
