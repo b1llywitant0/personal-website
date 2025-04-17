983f5b59632d4dd164706adc02167bb65519d6a5
b1llywitant0 <billywitanto@yahoo.co.id>
2025-04-17 17:13:34 +0700

    Chore: Prettier

--- src/components/Header/NavigationBar.tsx
+++ src/components/Header/NavigationBar.tsx
@@ -1,29 +1,26 @@
 import { Link } from 'react-router-dom'
-import {
-  NavigationMenu,
-  NavigationMenuList,
-} from '../ui/navigation-menu'
+import { NavigationMenu, NavigationMenuList } from '../ui/navigation-menu'
 import { Button } from '../ui/button'
 import LinkedInLogo from '../../assets/icons/linkedin-icon-black-png.png'
 import GithubLogo from '../../assets/icons/github-icon-black.png'
 import EmailIcon from '../../assets/icons/email-icon-black.png'
 import { NavItem } from './NavigationItem'
 
-export function NavigationBar() {    
+export function NavigationBar() {
   return (
     <div className="p-4 w-full urbanist-bold sticky top-0 text-base text-l">
       <div className="flex relative justify-center h-15 w-full isolate aspect-video rounded-xl bg-surface-light/50 shadow-lg ring-1 ring-black/5">
         <NavigationMenu className="w-full">
           <NavigationMenuList className="flex gap-x-10">
-            <NavItem to='/'>HOME</NavItem>
-            <NavItem to='/portfolios'>PORTFOLIOS</NavItem>
-            <NavItem to='/blogs'>BLOGS</NavItem>
+            <NavItem to="/">HOME</NavItem>
+            <NavItem to="/portfolios">PORTFOLIOS</NavItem>
+            <NavItem to="/blogs">BLOGS</NavItem>
           </NavigationMenuList>
         </NavigationMenu>
         <div className="absolute right-0 h-full px-5 flex items-center justify-end gap-5">
-            <Button className="p-0 bg-transparent hover:bg-transparent shadow-none">
+          <Button className="p-0 bg-transparent hover:bg-transparent shadow-none">
             <Link to="/contact">
-            <img src={EmailIcon} className="w-[28px] h-[28px]" />
+              <img src={EmailIcon} className="w-[28px] h-[28px]" />
             </Link>
           </Button>
           <Button className="p-0 bg-transparent hover:bg-transparent shadow-none">
--- src/components/Header/NavigationItem.tsx
+++ src/components/Header/NavigationItem.tsx
@@ -1,24 +1,31 @@
-import { Link, useLocation } from "react-router-dom";
-import { NavigationMenuItem, NavigationMenuLink } from "../ui/navigation-menu";
+import { Link, useLocation } from 'react-router-dom'
+import { NavigationMenuItem, NavigationMenuLink } from '../ui/navigation-menu'
 
-export function NavItem({ to, children }: { to: string; children: React.ReactNode }) {
-    const location = useLocation()
-    const isActive = location.pathname === to
-  
-    return (
-      <NavigationMenuItem
-        className={`w-30 rounded-md ${
-          isActive ? 'text-text-inverted bg-background-dark' : ''
-        }`}
-      >
-        <Link to={to}>
-          <NavigationMenuLink className={`flex items-center ${
+export function NavItem({
+  to,
+  children,
+}: {
+  to: string
+  children: React.ReactNode
+}) {
+  const location = useLocation()
+  const isActive = location.pathname === to
+
+  return (
+    <NavigationMenuItem
+      className={`w-30 rounded-md ${
+        isActive ? 'text-text-inverted bg-background-dark' : ''
+      }`}
+    >
+      <Link to={to}>
+        <NavigationMenuLink
+          className={`flex items-center ${
             isActive ? 'hover:bg-background-dark hover:text-text-inverted' : ''
-          }`}>
-            {children}
-          </NavigationMenuLink>
-        </Link>
-      </NavigationMenuItem>
-    )
-  }
-  
+          }`}
+        >
+          {children}
+        </NavigationMenuLink>
+      </Link>
+    </NavigationMenuItem>
+  )
+}
