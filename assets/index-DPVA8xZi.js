0c12341b3eec302ef98c1f4189f408f092f12997
b1llywitant0 <billywitanto@yahoo.co.id>
2025-04-21 22:03:38 +0700

    Updates

---
+++ assets/index-DPVA8xZi.js
@@ -0,0 +1,1 @@
+import{j as e,B as t}from"./index-DBEOWfJl.js";function s(){return e.jsx(e.Fragment,{children:e.jsx("div",{className:"flex flex-col items-center justify-center min-h-svh",children:e.jsx(t,{children:"Portfolios Click"})})})}export{s as Portfolios};
