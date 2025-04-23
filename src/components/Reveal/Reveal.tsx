96ff43cf90b5e939a7e4ec3bee956f9e65bf4b25
b1llywitant0 <billywitanto@yahoo.co.id>
2025-04-23 17:27:23 +0700

    Refactor: Adjusting job card

--- src/components/Reveal/Reveal.tsx
+++ src/components/Reveal/Reveal.tsx
@@ -5,9 +5,15 @@ interface RevealProps {
   children: React.ReactNode
   slide?: boolean
   duration?: number
+  vertical?: boolean
 }
 
-export function Reveal({children, slide=false, duration=0.5}: RevealProps) {
+export function Reveal({
+  children,
+  slide = false,
+  duration = 0.5,
+  vertical = true,
+}: RevealProps) {
   const targetRef = useRef(null)
   const isInView = useInView(targetRef, { once: true, amount: 0.2 })
   const mainControls = useAnimation()
@@ -19,48 +25,52 @@ export function Reveal({children, slide=false, duration=0.5}: RevealProps) {
       mainControls.start('visible')
       slideControls.start('visible')
     }
-  }, [isInView])
+  }, [isInView, mainControls, slideControls])
 
   return (
     <div ref={targetRef} className="relative w-fit overflow-hidden">
       <motion.div
-        variants={{
-          hidden: { opacity: 0, y: 75 },
-          visible: { opacity: 1, y: 0 },
-        }}
+        variants={
+          vertical
+            ? {
+                hidden: { opacity: 0, y: 75 },
+                visible: { opacity: 1, y: 0 },
+              }
+            : {
+                hidden: { opacity: 0, x: 75 },
+                visible: { opacity: 1, x: 0 },
+              }
+        }
         initial="hidden"
         animate={mainControls}
         transition={
-            slide ?
-            { duration: duration, delay: 0.25 }
-            :
-            { duration: duration }
+          slide ? { duration: duration, delay: 0.25 } : { duration: duration }
         }
       >
         {children}
       </motion.div>
-      {
-        slide ? 
-        <motion.div 
-            variants={{
-                hidden: { left: 0 },
-                visible: { left: '100%' },
-            }}
-            initial='hidden'
-            animate={slideControls}
-            transition={{ duration: duration, ease: 'easeIn' }}
-            style={{
-                position: 'absolute',
-                top: 4,
-                bottom: 4,
-                left: 0,
-                right: 0,
-                background: '#FFFFFF',
-                zIndex: 20,
-            }}
+      {slide ? (
+        <motion.div
+          variants={{
+            hidden: { left: 0 },
+            visible: { left: '100%' },
+          }}
+          initial="hidden"
+          animate={slideControls}
+          transition={{ duration: duration, ease: 'easeIn' }}
+          style={{
+            position: 'absolute',
+            top: 4,
+            bottom: 4,
+            left: 0,
+            right: 0,
+            background: '#FFFFFF',
+            zIndex: 20,
+          }}
         />
-        : ''
-      }
+      ) : (
+        ''
+      )}
     </div>
   )
 }
--- src/pages/home/Resume.tsx
+++ src/pages/home/Resume.tsx
@@ -10,6 +10,62 @@ import { Reveal } from '@/components/Reveal/Reveal'
 import { Job } from '@/components/Card/Job'
 
 const colors = ['#13FFAA', '#1E67C6', '#CE84CF', '#DD335C']
+const jobs = [
+  {
+    title: 'Software Engineer Trainee',
+    durationFrom: 'September 2024',
+    durationTo: 'February 2025',
+    company: 'Sea Labs Indonesia',
+    companyLink: 'https://sealabsindonesia.sea.com/home',
+    location: 'Jakarta, Indonesia',
+    description: 'Internship for full-stack software engineer.',
+    tools: [
+      'CLI',
+      'Git',
+      'PostgreSQL',
+      'Golang',
+      'Postman',
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'TypeScript',
+      'React+Vite',
+      'Tailwind',
+      'ShadCN',
+    ],
+  },
+  {
+    title: 'Digital Marketing Analyst',
+    durationFrom: 'October 2022',
+    durationTo: 'September 2024',
+    company: 'PT. Central Mega Kencana',
+    companyLink: 'https://centralmegakencana.com/',
+    location: 'Jakarta, Indonesia',
+    description:
+      'Responsible to analysis of digital marketing-related campaigns of all brands, which include but not limited to media placement of ads, website & search engine optimization, and social media.',
+    tools: [
+      'Python',
+      'Google Analytics 4',
+      'Google Tag Manager',
+      'Google Search Console',
+      'Ads Manager: Meta, Google, TikTok',
+      'SEMRush',
+      'FanpageKarma',
+      'Brand24',
+    ],
+  },
+  {
+    title: 'Research Assistant',
+    durationFrom: 'May 2018',
+    durationTo: 'September 2021',
+    company: 'Eijkman Institute of Molecular Biology',
+    companyLink:
+      'https://en.wikipedia.org/wiki/Eijkman_Molecular_Biology_Research_Center',
+    location: 'Jakarta, Indonesia',
+    description: 'Doing lab things related to Hepatitis B & C virus.',
+    tools: ['R', 'SPSS'],
+  },
+]
 
 export function Resume() {
   const driveFileId = '1NdFq6LhQEm5UpPPaWZel_9tjeKVRmfnK'
@@ -33,7 +89,7 @@ export function Resume() {
       className="h-[2000px] flex flex-col items-center justify-start gap-20"
     >
       <Reveal duration={1}>
-        <div className=" flex flex-col justify-start mt-30 gap-5 text-text-inverted text-2xl roboto-normal !font-light">
+        <div className=" flex flex-col justify-start mt-45 gap-5 text-text-inverted text-2xl roboto-normal !font-light">
           <div className="flex flex-row gap-5">
             <p>📍</p>
             <p>I am a data professional based in Jakarta, Indonesia.</p>
@@ -50,26 +106,30 @@ export function Resume() {
           </div>
         </div>
       </Reveal>
-      <div className='w-full flex flex-col gap-10 justify-center items-center text-text-inverted'>
+      <div className="w-full flex flex-col gap-10 justify-center items-center text-text-inverted">
         <Reveal slide={true}>
-          <span className="text-5xl inter-normal">
+          <span className="text-5xl inter-normal cursor-default">
             Work Experience
           </span>
         </Reveal>
-        <div>
-          <Reveal duration={1}>
-            <Job 
-              Title='Software Engineer Trainee'
-              DurationFrom='September 2024'
-              DurationTo='February 2025'
-              Company='Sea Labs Indonesia'
-              Location='Jakarta Pusat, Indonesia'
-              Description='Intership for full-stack software engineer'
-            />
-          </Reveal>
+        <div className="flex flex-col gap-10">
+          {jobs.map((item) => (
+            <Reveal duration={1} vertical={false}>
+              <Job
+                Title={item.title}
+                DurationFrom={item.durationFrom}
+                DurationTo={item.durationTo}
+                Company={item.company}
+                CompanyLink={item.companyLink}
+                Location={item.location}
+                Description={item.description}
+                Tools={item.tools}
+              />
+            </Reveal>
+          ))}
         </div>
       </div>
-      <div>
+      <Reveal>
         <a
           href={downloadUrl}
           target="_blank"
@@ -77,10 +137,10 @@ export function Resume() {
           className="rounded-md"
         >
           <Button className="cursor-pointer roboto-normal !font-light">
-            DOWNLOAD RESUME
+            You can download my resume here~
           </Button>
         </a>
-      </div>
+      </Reveal>
     </motion.section>
   )
 }
