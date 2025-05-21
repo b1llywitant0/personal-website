0a18d1afd457cf660768a2860a3193b92639486c
b1llywitant0 <billywitanto@yahoo.co.id>
2025-05-21 19:37:05 +0700

    Fix: Adjusting portfolio card

--- src/components/Card/Portfolio.tsx
+++ src/components/Card/Portfolio.tsx
@@ -17,9 +17,9 @@ export function PortfolioCard(props: PortfolioProps) {
         className="group w-screen max-w-[90%] h-[150px] md:w-[600px] md:h-[250px] mx-auto relative rounded-md bg-cover bg-center p-3 roboto-normal overflow-hidden"
         style={{ backgroundImage: `url(${props.img})` }}
       >
-        <div className="absolute inset-0 bg-black/60 translate-y-[65%] group-hover:translate-y-0 transition-transform duration-300 ease-in-out p-5 flex flex-col justify-between">
-          <div className="absolute top-4 flex flex-col">
-            <span className="font-bold text-[10px] md:text-[20px] h-[75px] w-fit">
+        <div className="absolute inset-0 bg-black/60 translate-y-[65%] group-hover:translate-y-0 transition-transform duration-300 ease-in-out px-5 py-3 flex flex-col justify-between">
+          <div className="flex flex-col">
+            <span className="font-bold text-[10px] md:text-[20px] h-[75px] w-full">
               {props.title}
             </span>
             <span className="font-light text-[13px]">
@@ -36,17 +36,16 @@ export function PortfolioCard(props: PortfolioProps) {
               }}
             />
           </div>
-          <div className="absolute bottom-4 hidden flex-row items-center justify-start gap-3 cursor-default flex-wrap md:flex">
-            {props.tools
-              ? props.tools.map((item) => (
-                  <div
-                    key={item.name}
-                    className="px-2 py-1 w-fit h-fit rounded-md bg-gray-500/90 text-[13px] hover:bg-gray-800"
-                  >
-                    {item.name}
-                  </div>
-                ))
-              : ''}
+
+          <div className="hidden md:flex flex-row items-center justify-start gap-2 cursor-default flex-wrap">
+            {props.tools?.map((item) => (
+              <div
+                key={item.name}
+                className="px-2 py-1 w-fit h-fit rounded-md bg-gray-500/90 text-[13px] hover:bg-gray-800"
+              >
+                {item.name}
+              </div>
+            ))}
           </div>
         </div>
       </div>
