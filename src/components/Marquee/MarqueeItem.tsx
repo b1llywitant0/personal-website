81d4981a0b34ce30d9ed819c1477527c929a7226
b1llywitant0 <billywitanto@yahoo.co.id>
2025-05-21 17:37:05 +0700

    Chore: Prettier

--- src/components/Marquee/MarqueeItem.tsx
+++ src/components/Marquee/MarqueeItem.tsx
@@ -7,8 +7,14 @@ export function MarqueeItem(props: MarqueItemProps) {
   return (
     <div className="inline-flex w-10 md:w-20 mx-4">
       <div className="flex flex-col items-center gap-3 w-full">
-        <img src={props.logo} className="h-8 md:h-10" alt={`logo-${props.name}`} />
-        <span className="roboto-normal text-[11px] md:text-[13px] mx-4">{props.name}</span>
+        <img
+          src={props.logo}
+          className="h-8 md:h-10"
+          alt={`logo-${props.name}`}
+        />
+        <span className="roboto-normal text-[11px] md:text-[13px] mx-4">
+          {props.name}
+        </span>
       </div>
     </div>
   )
